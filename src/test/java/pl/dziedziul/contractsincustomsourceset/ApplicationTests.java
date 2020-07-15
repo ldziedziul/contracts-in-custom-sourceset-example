b8c827fbb2d1b6dad9a201276c49b50a9df74451
Łukasz Dziedziul <l.dziedziul@gmail.com>
2020-07-15 12:53:38 +0200

    init commit

---
+++ src/test/java/pl/dziedziul/contractsincustomsourceset/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dziedziul.contractsincustomsourceset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
